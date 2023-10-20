db85a5d602e141c865f59ad8b3046b64dfde2ebb
Miguel De Jesus <deyjm008@mymail.unisa.edu.au>
2023-10-20 13:37:41 +1030

    started to implement the navbar to navigate between the pages

--- Assig2/a2reactapp/src/App.js
+++ Assig2/a2reactapp/src/App.js
@@ -1,24 +1,24 @@
+import { Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <div className="container-fluid">
+                  <Link className="navbar-brand" to="/">EnvData</Link>
+                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup">
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+              </div>
+              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div className="navbar-nav">
+                   <Link className="nav-link active" to="/Home">Home</Link>
+                  </div>
+              </div>
+          </nav>
+      </div>
   );
 }
 
