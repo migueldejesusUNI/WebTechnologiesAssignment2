3a0ed0ae31a1d27160054743333c9fd0c60069eb
Miguel De Jesus <deyjm008@mymail.unisa.edu.au>
2023-10-23 14:40:22 +1030

    updated the navbar to include the region and country page

--- Assig2/a2reactapp/src/App.js
+++ Assig2/a2reactapp/src/App.js
@@ -8,16 +8,18 @@ function App() {
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
               <div className="container-fluid">
                   <Link className="navbar-brand" to="/">EnvData</Link>
-                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup">
+                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDisplay">
                     <span className="navbar-toggler-icon"></span>
                   </button>
-              </div>
-              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                  <div className="navbar-nav">
-                   <Link className="nav-link active" to="/Home">Home</Link>
+                  <div className="collapse navbar-collapse" id="navbarDisplay">
+                      <div className="navbar-nav">
+                          <Link className="nav-link active" to="/Home">Home</Link>
+                          <Link className="nav-link" to="/Regions">Regions</Link>
+                          <Link className="nav-link" to="/Countries">Countries</Link>
+                      </div>
                   </div>
               </div>
-          </nav>
+          </nav> 
       </div>
   );
 }
