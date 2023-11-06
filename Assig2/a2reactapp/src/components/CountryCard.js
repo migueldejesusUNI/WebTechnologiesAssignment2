6b8908f99536833a66996de07abb416fb2f99122
Miguel De Jesus <deyjm008@mymail.unisa.edu.au>
2023-11-07 01:38:52 +1030

    displays message regarding if information is not available and got the country temperature details page to work

--- Assig2/a2reactapp/src/components/CountryCard.js
+++ Assig2/a2reactapp/src/components/CountryCard.js
@@ -8,10 +8,10 @@ const CountryCard = ({ countryId, countryName, iso3, imageUrl, cityCount, emissi
                 <h4 className="card-title">{countryName}</h4>
                 <h5 className="card-text">{iso3}</h5>
                 <hr />
-                <h6 className="card-text">Number of cities in {countryName}:  {cityCount}</h6>
+                <h6 className="card-text">{cityCount != 0 ? `Number of cities in ${countryName}:  ${cityCount}` : "No cities available"}</h6>
                 <hr /> 
-                <p>Contains emission records from {emissionDataYearRange[0]} to {emissionDataYearRange[1]} </p>
-                <p>Contains temperature records from {temperatureDataYearRange[0]} to {temperatureDataYearRange[1]}</p>
+                <p>{emissionDataYearRange[0] != 0 ? `Contains emission records from ${emissionDataYearRange[0]} to ${emissionDataYearRange[1]} ` : "No emission data available"} </p>
+                <p>{temperatureDataYearRange[0] != 0 ? `Contains temperature records from ${temperatureDataYearRange[0]} to ${temperatureDataYearRange[1]} ` : "No temperature data available"}</p>
                 <hr />
                 <div className="btn-container">
                     {cityCount > 0 ? (<Link to={"/Cities/" + countryId} className="btn btn-primary mb-1">Visit Cities </ Link >) : ("")}
--- Assig2/a2reactapp/src/routes/CountryTemperatureDetail.js
+++ Assig2/a2reactapp/src/routes/CountryTemperatureDetail.js
@@ -7,11 +7,9 @@ const CountryTemperatureDetail = ({ }) => {
 
     let params = useParams();
 
-    const [tempData, settempData] = useState({
-        "rawTemperatureData": []
-    });
+    const [tempData, settempData] = useState({ });
     const [countryId, setCountryId] = useState(params.countryId)
-
+ 
 
     useEffect(() => {
         console.log("useEffect");
@@ -26,51 +24,48 @@ const CountryTemperatureDetail = ({ }) => {
     
     return (
         <div>
-            <h2 className="text-center">
-            Country Temperature detail
-            </h2>
-            <p>
-                Country ID: {countryId}
-            </p>
-            <p>Earliest Year: {tempData.minYear}</p>
-            <p>Latest Year: {tempData.maxYear}</p>
-
-            
-
+            <div class="text-white bg-gradient bg-success p-2 my-2 border rounded" >
+                <h2 className="text-center">
+                    Country Temperature
+                </h2>
+            </div>
+            <hr />
+            <Link to={"/Countries/0"} className="btn btn-primary mb-1">Back to Country List</Link>
+            <hr />
             {!tempData.rawTemperatureData?.length == 0 ? (
-                <div className="row justify-content-center">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Year</th>
-                                <th>Unit</th>
-                                <th>Change</th>
-                                <th>Value</th>
-                                <th>RegionalAvg</th>
-                                <th>RegionalMin</th>
-                                <th>RegionalMax</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {tempData.rawTemperatureData?.map((obj) => (
+                <div className="row justify-content-center" >
+                    <div className="table-responsive">    
+                        <table className="table table-striped table-bordered">
+                            <thead className="table-dark">
+                                <tr>
+                                    <th>Year</th>
+                                    <th>Unit</th>
+                                    <th>Change</th>
+                                    <th>Value</th>
+                                    <th>RegionalAvg</th>
+                                    <th>RegionalMin</th>
+                                    <th>RegionalMax</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {tempData.rawTemperatureData?.map((obj) => (
                             
-                            <tr>
-                                <td>{obj.theCountryTempData.year}</td>
-                                <td>{obj.theCountryTempData.unit}</td>
-                                <td>{obj.theCountryTempData.change}</td>
-                                <td>{obj.theCountryTempData.value}</td>
-                                <td>{obj.regionalAvg}</td>
-                                <td>{obj.regionalMin}</td>
-                                <td>{obj.regionalMax}</td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
+                                <tr>
+                                    <td>{obj.theCountryTempData.year != null ? obj.theCountryTempData.year : "N/A"}</td>
+                                    <td>{obj.theCountryTempData.unit != null ? obj.theCountryTempData.unit : "N/A"}</td>
+                                    <td>{obj.theCountryTempData.change != null ? obj.theCountryTempData.change : "N/A"}</td>
+                                    <td>{obj.theCountryTempData.value != null ? obj.theCountryTempData.value : "N/A"}</td>
+                                    <td>{obj.regionalAvg != null ? obj.regionalAvg : "N/A"}</td>
+                                    <td>{obj.regionalMin != null ? obj.regionalMin : "N/A"}</td>
+                                    <td>{obj.regionalMax != null ? obj.regionalMax : "N/A"}</td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             ) : (<h2>No data found</h2>)}
-            
-               
-            
+                     
         </div>
     )
 }
