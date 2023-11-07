4d20291dd95d9acbabbd080df834f22a26ac17f2
Miguel De Jesus <deyjm008@mymail.unisa.edu.au>
2023-11-07 13:38:35 +1030

    city search bar now works

--- Assig2/a2reactapp/src/components/CityCardList.js
+++ Assig2/a2reactapp/src/components/CityCardList.js
@@ -8,19 +8,35 @@ const CityCardList = ({ }) => {
 
     const [cardData, setState] = useState([]);
     const [countryId, setCountryId] = useState(params.countryId);
+    const [query, setQuery] = useState('');
 
     useEffect(() => {
         console.log("useEffect")
-        fetch(`http://localhost:5256/api/C_Cities/${countryId}`)
+        fetch(`http://localhost:5256/api/C_Cities/${countryId}?searchText=${query}`)
         .then(response => response.json())
             .then(data => setState(data))
             .catch(err => {
                 console.log(err);
             })
-    }, [countryId])
+    }, [countryId, query])
+
+    function searchQuery(evt) {
+        const value = document.querySelector(['[name="searchText"]']).value;
+        setQuery(value);
+    }
 
     return (
         <div className="row justify-content-center">
+            <div className="row col-3 text-center">
+                <input type="text" name="searchText" className="form-control" placeholder="Search for cities"/>
+            </div>
+            <div className="row col-3 text-left">
+                <button type="button" className="btn btn-primary" onClick={searchQuery}>Search</button>
+            </div>
+            <div className="row justify-content-center mb-3">
+                <Link to={"/Countries/0" } className="btn btn-primary">Back to Countries</Link>
+            </div>
+            <hr /> 
             {cardData?.map((obj) => (
                 <CityCard
                     cityId={obj.cityId}
